1a8e3d564d4b3d910c7ef7f3e9978b41127ce6ec
Glyn Owen Hanmer <glynhanmer@gmail.com>
2020-11-28 21:08:36 +0000

    Add buildCmdTree function

---
+++ cmd/osc-proto/build.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"io"
+
+	"github.com/glynternet/pkg/log"
+	"github.com/spf13/cobra"
+)
+
+func buildCmdTree(logger log.Logger, out io.Writer, rootCmd *cobra.Command) {
+
+}
