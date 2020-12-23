c1640c2d9a4b68284a31f4ee97ed43d97368c92b
Glyn Owen Hanmer <glynhanmer@gmail.com>
2020-12-23 11:48:47 +0000

    ♻️ extract test outputs to files to ease of maintenance

--- pkg/generate/generatetest/util.go
+++ pkg/generate/generatetest/util.go
@@ -7,13 +7,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func AssertEqualContentLayout(t *testing.T, expectedContentLayout map[string]string, actual map[string][]byte) {
+func AssertEqualContentLayout(t *testing.T, expectedContentLayout map[string][]byte, actual map[string][]byte) {
 	require.Equal(t, len(expectedContentLayout), len(actual))
 	for path, expectedContent := range expectedContentLayout {
 		actualContent, exists := actual[path]
 		if !exists {
 			t.Fatalf("expectedContentLayout path %q does not exist in actual %+v", path, actual)
 		}
-		assert.Equal(t, expectedContent, string(actualContent))
+		assert.Equal(t, string(expectedContent), string(actualContent))
 	}
 }
